51f4b145f96b82e2518196acde20301ab341f31d
Shiv Raj Badu <userrails@gmail.com>
2017-10-06 11:42:32 +0545

    initial commit

---
+++ config/initializers/quickbooks.rb
@@ -0,0 +1,9 @@
+OAUTH_CONSUMER_KEY = "OAUTH_CONSUMER_KEY"
+OAUTH_CONSUMER_SECRET = "OAUTH_CONSUMER_SECRET"
+
+::QB_OAUTH_CONSUMER = OAuth::Consumer.new(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, {
+    :site                 => "https://oauth.intuit.com",
+    :request_token_path   => "/oauth/v1/get_request_token",
+    :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
+    :access_token_path    => "/oauth/v1/get_access_token"
+})
