0ed3734b55b3f9631f54422eba36fc032f242d35
Shiv Raj Badu <userrails@gmail.com>
2017-10-11 11:43:47 +0545

    add filter code, not working

--- app/controllers/purchase_orders_controller.rb
+++ app/controllers/purchase_orders_controller.rb
@@ -1,6 +1,17 @@
 class PurchaseOrdersController < ApplicationController
   def index
-    @purchase_orders = qbo_api.all(:purchase_orders)
+    @purchase_orders = case params[:status]
+                       when 'Open'
+                         qbo_api.query(
+                           "SELECT * FROM PurchaseOrder WHERE POStatus=Open"
+                         )
+                       when 'Closed'
+                         qbo_api.query(
+                           "SELECT * FROM PurchaseOrder WHERE POStatus=Closed"
+                         )
+                       else
+                         qbo_api.all(:purchase_orders)
+                       end
   end
 
   def show
